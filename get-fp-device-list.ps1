2073bc80848bae0158892a902cc36de77867305e
john-babio <johndbabio@gmail.com>
2020-11-17 15:59:26 -0500

    Update get-fp-device-list.ps1

--- get-fp-device-list.ps1
+++ get-fp-device-list.ps1
@@ -1,10 +1,22 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$acctname = " "
-$password = " "
-$server = " "
-$domainuuid = " "
+add-type @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class TrustAllCertsPolicy : ICertificatePolicy {
+        public bool CheckValidationResult(
+            ServicePoint srvPoint, X509Certificate certificate,
+            WebRequest request, int certificateProblem) {
+            return true;
+        }
+    }
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+$acctname = "username"
+$password = "password"
+$server = "ip address of FMC"
+#$domainuuid can be found by going to https://yourfmcip/api/api-explorer and running execute on any device HTTP Get request.
+$domainuuid = "uuid of domain found in api explorer"
 $auth = Invoke-WebRequest -Method POST  "https://$server/api/fmc_platform/v1/auth/generatetoken" -Header @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("$($acctname):$($password)"));}
 $authkey = $auth.Headers.'X-auth-access-token'
-$apipath = "/api/fmc_config/v1/domain/"$domainuuid"?limit=250"
-$GetRequest = Invoke-RestMethod -Method GET -ContentType 'application/json' "https://$server/$apipath" -Header @{'X-auth-access-token' = "$authkey"}
-$GetRequest.items.Name 
+$apipath = "/api/fmc_config/v1/domain/$domainuuid/devices/devicerecords?limit=300&expanded=true"
+$network = Invoke-RestMethod -Method GET -ContentType 'application/json' "https://$server/$apipath" -Header @{'X-auth-access-token' = "$authkey"} 
+$network.items 
